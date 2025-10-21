e77454754378dffb1dbe30bc99ca95f37acb31f7
vodaiphuoc <phuocvodn98@gmail.com>
2025-10-22 03:40:48 +0700

    add type for init upload

--- commons/src/upload.model.ts
+++ commons/src/upload.model.ts
@@ -1,23 +1,55 @@
-import * as z from "zod"; 
+import * as z from "zod";
 
 /**
- * Schema for validation custom headers sent with each chunk request
+ * Schema for validation custom headers sent in **Initial** upload request
  * on backend
  */
-// export const ChunkUploadHeadersSchema = z.record(z.literal(requireHeaderFiled), z.string());
+export const InitUploadHeadersSchema = z.object({
+    'user-id': z.string(),
+    'content-type': z.string('application/json')
+});
 
-export const ChunkUploadHeadersSchema = z.object({
-    'file-id': z.string().regex(/[0-9]/),
+/**
+ * Type for the required custom headers sent in **Initial** upload request
+ * @type
+ */
+export type InitUploadHeaders = z.infer<typeof InitUploadHeadersSchema>;
+
+
+/**
+ * Schema for validation body sent in **Initial** upload request
+ * on backend
+ */
+export const InitUploadBodySchema = z.object({
     'file-name': z.string(),
+    'mine-type': z.string(),
+    'file-size': z.string().regex(/[0-9]/)
+});
+
+/**
+ * Type for the required body sent in **Initial** upload request
+ * @type
+ */
+export type InitUploadBody = z.infer<typeof InitUploadBodySchema>;
+
+export interface InitUploadReponse {
+    fileId: string
+}
+
+/**
+ * Schema for validation custom headers sent with each **chunk upload** request
+ * on backend
+ */
+export const ChunkUploadHeadersSchema = z.object({
+    'file-id': z.uuidv4(),
     'content-range': z.string().regex(/^bytes/),
     'content-type': z.string('application/octet-stream'),
     'chunk-index': z.string(),
     'total-chunks': z.string().min(1)
-})
-
+});
 
 /**
- * Type for the required custom headers sent with each chunk request.
+ * Type for the required custom headers sent with each **chunk upload** request.
  * @type
  */
 export type ChunkUploadHeaders = z.infer<typeof ChunkUploadHeadersSchema>;
