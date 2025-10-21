45ded85ec66dd42f66a4c318dcbc95144f2402b2
vodaiphuoc <phuocvodn98@gmail.com>
2025-10-21 13:52:30 +0700

    schema for header only

--- commons/src/upload.model.ts
+++ commons/src/upload.model.ts
@@ -1,12 +1,24 @@
-type requireHeaderFiled = 'File-Id' | 'File-Name' | 'Content-Range' | 'Content-Type' | 'Chunk-Index' | 'Total-Chunks';
+import * as z from "zod"; 
+
+/**
+ * Schema for validation custom headers sent with each chunk request
+ * on backend
+ */
+// export const ChunkUploadHeadersSchema = z.record(z.literal(requireHeaderFiled), z.string());
+
+export const ChunkUploadHeadersSchema = z.object({
+    'file-id': z.string().regex(/[0-9]/),
+    'file-name': z.string(),
+    'content-range': z.string().regex(/^bytes/),
+    'content-type': z.string('application/octet-stream'),
+    'chunk-index': z.string(),
+    'total-chunks': z.string().min(1)
+})
+
 
 /**
  * Type for the required custom headers sent with each chunk request.
  * @type
  */
-export type ChunkUploadHeaders = Record<requireHeaderFiled, string>
+export type ChunkUploadHeaders = z.infer<typeof ChunkUploadHeadersSchema>;
 
-/**
- * The body is the raw binary data (a Buffer in Express).
- */
-export type ChunkUploadBody = Buffer;
