ce8d3ab8d8192358aa47b39c52976efe0dda9273
vodaiphuoc <phuocvodn98@gmail.com>
2025-10-27 01:53:46 +0700

    add factory logger

---
+++ upload-service/src/utils/logger.ts
@@ -0,0 +1,40 @@
+import * as winston from 'winston';
+import * as path from 'path';
+
+const consoleFormat = winston.format.combine(
+    winston.format.colorize(),
+    winston.format.printf(info => {
+        const module = info['moduleName'] || 'APP';
+        return `[${info['timestamp']}] [${module}] ${info.level}: ${info.message}`;
+    })
+);
+
+const transports = [
+    new winston.transports.Console({
+        format: consoleFormat,
+    }),
+    new winston.transports.File({
+        filename: 'error.log',
+        level: 'error'
+    }),
+];
+
+/**
+ * Factory logger per module
+ * @param modulePath 
+ * @returns 
+ */
+export function getLogger(modulePath: string): winston.Logger {
+    const moduleName = path.basename(modulePath);
+
+    return winston.createLogger({
+        level: 'info',
+        format: winston.format.combine(
+            winston.format.timestamp({
+                format: 'YYYY-MM-DD HH:mm:ss'
+            }),
+            winston.format.label({ label: moduleName })
+        ),
+        transports: transports,
+    });
+}
