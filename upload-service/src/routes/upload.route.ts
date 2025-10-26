a99383fc1ca0a15c5db684d31c434c521e7cf0ce
vodaiphuoc <phuocvodn98@gmail.com>
2025-10-27 01:53:22 +0700

    update route

--- upload-service/src/routes/upload.route.ts
+++ upload-service/src/routes/upload.route.ts
@@ -6,11 +6,17 @@ import {
     rawBodyParser
 } from '@controllers/upload.controller';
 
+import {
+    validateInitUploadBody,
+    validateInitUploadHeader,
+    validateChunkHeader
+} from '@middlewares/upload.middleware';
+
 const uploadRouter = express.Router();
 
 // Apply the rawBodyParser middleware ONLY to the chunk upload route
-uploadRouter.post('/init', handleInitUpload);
-uploadRouter.post('/chunk-upload', rawBodyParser, handleChunkUpload);
+uploadRouter.post('/init', validateInitUploadHeader, validateInitUploadBody, handleInitUpload);
+uploadRouter.post('/chunk-upload', rawBodyParser, validateChunkHeader, handleChunkUpload);
 uploadRouter.post('/complete', express.json(), finalizeUpload);
 
 export default uploadRouter;
